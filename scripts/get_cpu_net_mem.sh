3293c83f900d555dd27ac7da0ade446867663923
lewzylu <328774225@qq.com>
2018-03-28 12:09:18 +0800

    Add script deal with storage_data

---
+++ scripts/get_cpu_net_mem.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+Time=1
+#CPU0
+CPULOG_1=`cat /proc/stat | head -n1 | awk '{print $2" "$3" "$4" "$5" "$6" "$7" "$8}'`
+SYS_IDLE_1=`echo $CPULOG_1 | awk '{print $4}'`
+Total_1=`echo $CPULOG_1 | awk '{print $1+$2+$3+$4+$5+$6+$7}'`
+
+#NET0
+TE1=`date --utc`
+RX1=`cat /proc/net/dev | grep eth0 |awk '{print $2}'`
+TX1=`cat /proc/net/dev | grep eth0 |awk '{print $10}'`
+
+sleep $Time
+
+#CPU1
+CPULOG_2=`cat /proc/stat | head -n1 | awk '{print $2" "$3" "$4" "$5" "$6" "$7" "$8}'`
+SYS_IDLE_2=`echo $CPULOG_2 | awk '{print $4}'`
+Total_2=`echo $CPULOG_2 | awk '{print $1+$2+$3+$4+$5+$6+$7}'`
+Total=`expr $Total_2 - $Total_1`
+SYS_IDLE=`expr $SYS_IDLE_2 - $SYS_IDLE_1`
+SYS_USAGE=`echo $SYS_IDLE/$Total*100 |bc -l`
+SYS_Rate=`echo 100-$SYS_USAGE |bc -l`
+Disp_SYS_Rate=`expr "scale=2; $SYS_Rate/1" |bc`
+
+printf "%4.2f\n" ${Disp_SYS_Rate}
+
+#NET1
+DATE2=`date --utc`
+RX2=`cat /proc/net/dev | grep eth0 |awk '{print $2}'`
+TX2=`cat /proc/net/dev | grep eth0 |awk '{print $10}'`
+RX=$(( $RX2-$RX1 ))
+TX=$(( $TX2-$TX1 ))
+RXBAND=$(($RX*8/$Time))
+TXBAND=$(($TX*8/$Time)) 
+SX=$(( $RX+$TX ))
+SXBAND=`echo $SX*8/$Time | bc -l`
+printf  "%.0f\n" ${SXBAND}
+
+#MEM
+LoadMemory=$(cat /proc/meminfo | awk '{print $2}')
+Total=$(echo $LoadMemory | awk '{print $1}')
+Free1=$(echo $LoadMemory | awk '{print $2}')
+Free2=$(echo $LoadMemory | awk '{print $3}')
+Free3=$(echo $LoadMemory | awk '{print $4}')
+ 
+Used=`expr $Total - $Free1 - $Free2 - $Free3`
+Used_Rate=`expr  $Used/$Total*100 | bc -l`
+Memory_Used_Rate=`expr  $Used_Rate/1 | bc`
+echo $Memory_Used_Rate%
--- scripts/storage_data.py
+++ scripts/storage_data.py
@@ -1,5 +1,26 @@
 # -*- coding: utf-8 -*-
 import os
-f = os.popen('df dev/md0')
-rt = f.read().split('\n')
-in(a[1].split()).split(' ')[1]
+import time
+
+def get_space_data(path):
+	total_space = os.popen("df "+path+" | grep "+path+" | awk '{print $2}'").read().split('\n')[0]
+	used_space = os.popen("df "+path+" | grep "+path+" | awk '{print $3}'").read().split('\n')[0]
+	return used_space,total_space
+
+def get_cpu_net_mem():
+	f = os.popen('sh get_cpu_net_mem.sh')
+	rt = f.read().split('\n')
+	cpu = rt[0]
+	net = rt[1]
+	mem = rt[2]
+	return cpu,net,mem
+
+if __name__ == "__main__":
+	while(True):	
+		t = int(time.time())
+		time.sleep(0.5)
+		if t % 60 == 0:
+			used_space, total_space =  get_space_data('/dev')
+			cpu, net, mem = get_cpu_net_mem()
+			print cpu,net,mem	
+#print get_netwidth()
