3773572b3a7a99131bb0e3e46dd04b7b6535ff94
lewzylu <327874225@qq.com>
2018-04-02 21:42:32 +0800

    fix storagedata# On branch master

--- cdm_data/test2.py
+++ cdm_data/test2.py
@@ -7,12 +7,11 @@ import json
 s = requests.session()
 data= '{"username": "admin", "password": ""}'
 print json.loads(data)
-headers = {"csrf_token":"123213"}
-rt =s.post("http://127.0.0.1:8080/login",data=data)
+rt =s.post("http://127.0.0.1:8000/login",data=data)
 # Create your tests here.
-#print rt.text
+print rt.text
  
-#rt =s.post("http://127.0.0.1:8080/login",data=data)
+rt =s.get("http://127.0.0.1:8000/storagedata")
 # Create your tests here.
 print rt.text
 #rt =s.post("http://127.0.0.1:8099/logout")
